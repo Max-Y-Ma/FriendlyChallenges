8d512f436218d9a2f2fc5b8099f02a9af093f13e
Max Ma <maxma2@illinois.edu>
2022-11-22 15:25:40 -0600

    Working JPG Output

--- ImageProcessing/Solution/ImageProcessing.cpp
+++ ImageProcessing/Solution/ImageProcessing.cpp
@@ -10,97 +10,132 @@ void processImage(char* file) {
     outfile.open("./Output/output.jpg", std::ios_base::binary);
 
     // Grab binary data from JPG File
-    std::vector<int> data = std::vector<int>(std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>());
+    std::vector<char> data = std::vector<char>(std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>());
     
     // Process data to end of JPG File
     int position {0};
-    int marker {0};
+    char marker {0};
     do {
-        marker = getMarker(data, &position);
+        marker = read_int8(data, &position);
 
         switch (marker)
         {
-        case START_OF_IMAGE:
-            processSOI(data, &position, outfile);
-            break;
-        case APPLICATION_0:
-            processApp(data, &position, outfile);
-            break;
-        case QUANTIZATION_TABLE:
-            processQuantTable(data, &position, outfile);
-            break;
-        case START_OF_FRAME:
-            processSOF(data, &position, outfile);
-            break;
-        case HUFFMAN_TABLE:
-            processHTable(data, &position, outfile);
-            break;
-        case START_OF_SCAN:
-            processSOS(data, &position, outfile);
-            break;
+        // case START_OF_IMAGE:
+        //     processSOI(data, &position, outfile);
+        //     break;
+        // case APPLICATION_0:
+        //     processApp(data, &position, outfile);
+        //     break;
+        // case QUANTIZATION_TABLE:
+        //     processQuantTable(data, &position, outfile);
+        //     break;
+        // case START_OF_FRAME:
+        //     processSOF(data, &position, outfile);
+        //     break;
+        // case HUFFMAN_TABLE:
+        //     processHTable(data, &position, outfile);
+        //     break;
+        // case START_OF_SCAN:
+        //     processSOS(data, &position, outfile);
+        //     break;
         default:
-            std::cout << "DEBUG MESSAGE: Invalid Marker" << std::endl;
+            outfile << marker;
+            //std::cout << "DEBUG MESSAGE: Invalid Marker" << std::endl;
             break;
         }
 
     } while (marker != END_OF_IMAGE);
+    // Output to Marker to JPG
+    outfile << END_OF_IMAGE;
 
     infile.close();
 }
 
-void printNum(int n) {
+void printNum(uint16_t n) {
     std::cout << std::setw(2) << std::setfill('0') << std::hex << n << std::endl;
 }
 
-uint32_t getMarker(std::vector<int>& data, int* position) {
-    uint32_t temp {0};
-    for (int i {0}; i < SIZE_OF_MARKER; i++) {
-        // Left Shift 8
-        temp = temp << BYTE;
-        // Set Lowest 8 Bits
-        temp = temp | ((data).at(i + *position) & 0xff);
-    }
-    // Update position
-    *position += SIZE_OF_MARKER;
-    return temp;
-}
-
-void processSOI(std::vector<int>& data, int* position, std::ofstream& outfile) {
+void processSOI(std::vector<char>& data, int* position, std::ofstream& outfile) {
     std::cout << "DEBUG MESSAGE: Processing Start of Image" << std::endl;
     // Output to Marker to JPG
     outfile << START_OF_IMAGE;
 }
 
-void processApp(std::vector<int>& data, int* position, std::ofstream& outfile) {
+void processApp(std::vector<char>& data, int* position, std::ofstream& outfile) {
     std::cout << "DEBUG MESSAGE: Processing Application 0..." << std::endl;
 
     // Output to Marker to JPG
     outfile << APPLICATION_0;
 
-    // Create Struct for this
-    uint16_t length {0};
-    uint32_t id_1 {0};
-    uint8_t id_2 {0};
+    // Create and store data
+    application_0 app_0;
+    app_0.length = read_int16(data, position);
+    app_0.id_1 = read_int32(data, position);
+    app_0.id_2 = read_int8(data, position);
+    app_0.version = read_int16(data, position);
+    app_0.units = read_int8(data, position);
+    app_0.density = read_int32(data, position);
+    app_0.thumbnail = read_int16(data, position);
 
+    // Display Data 
+    std::cout << "Length: " << std::bitset<16>(app_0.length) << std::endl;
+    std::cout << "ID 1: " << std::bitset<32>(app_0.id_1) << std::endl;
+    std::cout << "ID 2: " << std::bitset<8>(app_0.id_2) << std::endl;
+    std::cout << "Version: " << std::bitset<16>(app_0.version) << std::endl;
+    std::cout << "Units: " << std::bitset<8>(app_0.units) << std::endl;
+    std::cout << "Density: " << std::bitset<32>(app_0.density) << std::endl;
+    std::cout << "Thumbnail: " << std::bitset<16>(app_0.thumbnail) << std::endl;
 
-
-    length = ((data.at(*position) & 0xff) << BYTE) | (data.at(*position + 1) & 0xff);
-    *position += SIZE_OF_LENGTH;
-    printNum(length);
+    // Output Data to JPG
+    outfile << app_0.length << app_0.id_1 << app_0.id_2 << app_0.version 
+        << app_0.units << app_0.density << app_0.thumbnail;
 }
 
-void processQuantTable(std::vector<int>& data, int* position, std::ofstream& outfile) {
+void processQuantTable(std::vector<char>& data, int* position, std::ofstream& outfile) {
     std::cout << "DEBUG MESSAGE: Processing Quantization Table..." << std::endl;
+
+    // Output to Marker to JPG
+    outfile << QUANTIZATION_TABLE;
 }
 
-void processSOF(std::vector<int>& data, int* position, std::ofstream& outfile) {
+void processSOF(std::vector<char>& data, int* position, std::ofstream& outfile) {
     std::cout << "DEBUG MESSAGE: Processing Start of Frame..." << std::endl;
+
+    // Output to Marker to JPG
+    outfile << START_OF_FRAME;
 }
 
-void processHTable(std::vector<int>& data, int* position, std::ofstream& outfile) {
+void processHTable(std::vector<char>& data, int* position, std::ofstream& outfile) {
     std::cout << "DEBUG MESSAGE: Processing Huffman Table..." << std::endl;
+
+    // Output to Marker to JPG
+    outfile << HUFFMAN_TABLE;
 }
 
-void processSOS(std::vector<int>& data, int* position, std::ofstream& outfile) {
+void processSOS(std::vector<char>& data, int* position, std::ofstream& outfile) {
     std::cout << "DEBUG MESSAGE: Processing Start of Scan..." << std::endl;
+
+    // Output to Marker to JPG
+    outfile << START_OF_SCAN;
+}
+
+u_int8_t read_int8(std::vector<char>& data, int* position) {
+    u_int8_t temp = (data.at(*position) & 0xff);
+    *position += SIZE_OF_U8INT;
+    return temp;
+}
+
+uint16_t read_int16(std::vector<char>& data, int* position) {
+    uint16_t temp = ((data.at(*position) & 0xff) << BYTE) | (data.at(*position + 1) & 0xff);
+    *position += SIZE_OF_U16INT;
+    return temp;
+}
+
+uint32_t read_int32(std::vector<char>& data, int* position) {
+    uint16_t temp = ((data.at(*position) & 0xff) << BYTE * 3) | 
+                    ((data.at(*position + 1) & 0xff) << BYTE * 2) |
+                    ((data.at(*position + 2) & 0xff) << BYTE) |
+                    (data.at(*position + 3) & 0xff);
+    *position += SIZE_OF_U32INT;
+    return temp;
 }
--- ImageProcessing/Solution/ImageProcessing.h
+++ ImageProcessing/Solution/ImageProcessing.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <vector>
 #include <iomanip>
+#include <bitset>
 
 // Defines
 #define START_OF_IMAGE          0xFFD8
@@ -16,18 +17,69 @@
 #define START_OF_SCAN           0xFFDA
 #define END_OF_IMAGE            0xFFD9
 #define SIZE_OF_MARKER          0x0002
-#define SIZE_OF_LENGTH          0x0002
+#define SIZE_OF_U8INT           0x0001
+#define SIZE_OF_U16INT          0x0002
+#define SIZE_OF_U32INT          0x0004
 #define BYTE                    0x0008
+
+// Datatypes
+struct {
+    u_int16_t   length;
+    u_int32_t   id_1;
+    u_int8_t    id_2;
+    u_int16_t   version;
+    u_int8_t    units;
+    u_int32_t   density;
+    u_int16_t   thumbnail;
+} typedef application_0;
+
+struct {
+    u_int16_t           length;
+    u_int8_t            destination;
+    u_int8_t            table[8][8];
+} typedef quantization_table;
+
+struct {
+    u_int16_t           length;
+    u_int16_t           precision;
+    u_int16_t           line_nb;
+    u_int8_t            samples;
+    u_int8_t            components;
+    u_int8_t            id_1;
+    u_int8_t            factor_1;
+    u_int8_t            table_1;
+    u_int8_t            id_2;
+    u_int8_t            factor_2;
+    u_int8_t            table_2;
+    u_int8_t            id_3;
+    u_int8_t            factor_3;
+    u_int8_t            table_3;
+} typedef start_of_frame;
+
+struct {
+    u_int16_t           length;
+    u_int8_t            class_destination;
+    u_int8_t            table[32] ;
+} typedef huffman_table;
+
+struct {
+    u_int16_t           length;
+    u_int8_t            components;
+    u_int8_t            table[16];
+    u_int8_t            data[64];
+} typedef start_of_scan;
 
 // Function Prototypes
 void printNum(int n);
 void processImage(char* file);
-uint32_t getMarker(std::vector<int>& data, int* position);
-void processSOI(std::vector<int>& data, int* position, std::ofstream& outfile);
-void processApp(std::vector<int>& data, int* position, std::ofstream& outfile);
-void processQuantTable(std::vector<int>& data, int* position, std::ofstream& outfile);
-void processSOF(std::vector<int>& data, int* position, std::ofstream& outfile);
-void processHTable(std::vector<int>& data, int* position, std::ofstream& outfile);
-void processSOS(std::vector<int>& data, int* position, std::ofstream& outfile);
+void processSOI(std::vector<char>& data, int* position, std::ofstream& outfile);
+void processApp(std::vector<char>& data, int* position, std::ofstream& outfile);
+void processQuantTable(std::vector<char>& data, int* position, std::ofstream& outfile);
+void processSOF(std::vector<char>& data, int* position, std::ofstream& outfile);
+void processHTable(std::vector<char>& data, int* position, std::ofstream& outfile);
+void processSOS(std::vector<char>& data, int* position, std::ofstream& outfile);
+uint8_t read_int8(std::vector<char>& data, int* position);
+uint16_t read_int16(std::vector<char>& data, int* position);
+uint32_t read_int32(std::vector<char>& data, int* position);
 
 #endif
